e549615eff4cfec7784096edf965985e55b55e87
Giuseppe Burtini <joe@truephp.com>
2017-04-16 14:23:12 -0700

    Prettier.

--- test/evaluators.spec.js
+++ test/evaluators.spec.js
@@ -26,13 +26,28 @@ document.body.appendChild(parentDiv);
 describe('evaluators', () => {
   describe('evaluateXPath', () => {
     it('should return STRING_TYPE from a simple DOM query.', () => {
-      const xpath = evaluators.evaluateXPath(document, '/html/body/div/div[1]', document, XPathResult.STRING_TYPE);
-      expect(xpath).to.be.equal('Hello world.', 'Expected inner string to be returned');
+      const xpath = evaluators.evaluateXPath(
+        document,
+        '/html/body/div/div[1]',
+        document,
+        XPathResult.STRING_TYPE
+      );
+      expect(xpath).to.be.equal(
+        'Hello world.',
+        'Expected inner string to be returned'
+      );
     });
 
     it('should return Node from a simple DOM query.', () => {
-      const xpath = evaluators.evaluateXPath(document, '/html/body/div/div[1]', document);
-      expect(xpath).to.be.deep.equal([childDiv1], 'Expected array of matching elements to be returned');
+      const xpath = evaluators.evaluateXPath(
+        document,
+        '/html/body/div/div[1]',
+        document
+      );
+      expect(xpath).to.be.deep.equal(
+        [childDiv1],
+        'Expected array of matching elements to be returned'
+      );
     });
   });
 
@@ -41,32 +56,34 @@ describe('evaluators', () => {
       const result = evaluators.getElementsBySelector(document, '.test');
       expect(result).to.have.length(1);
       expect(result[0].className).to.be.eq('test');
-    })
+    });
   });
-  
+
   describe('getElementsByXPath', () => {
     it('should support a similar lookup by XPath rule', () => {
       const result = evaluators.getElementsByXPath(document, '//*[@id="test"]');
       expect(result).to.have.length(1);
       expect(result[0].id).to.be.eq('test');
-    })
+    });
     it('should work in the no results case', () => {
       const result = evaluators.getElementsByXPath(document, '//*[@id="abc"]');
       expect(result).to.have.length(0);
-    })
+    });
     it('should work in the invalid selector', () => {
       const result = evaluators.getElementsByXPath(document, '/1!@*#');
       expect(result).to.have.length(0);
-    })
+    });
   });
-  
+
   describe('getRuleMatchingElements', () => {
     it('should support a similar lookup by CSS rule', () => {
       // NOTE: opposite parameter order originates in Mozilla code.
-      const result = evaluators.getRuleMatchingElements({ selectorText: '.test' }, document);
+      const result = evaluators.getRuleMatchingElements(
+        {selectorText: '.test'},
+        document
+      );
       expect(result).to.have.length(1);
       expect(result[0].className).to.be.eq('test');
-    })
+    });
   });
 });
-
--- test/generators.spec.js
+++ test/generators.spec.js
@@ -9,9 +9,7 @@ function newDocument() {
   return doc;
 }
 
-function simpleDocument() {
-
-}
+function simpleDocument() {}
 
 describe('generators', () => {
   describe('getElementTreeXPath', () => {
@@ -28,18 +26,30 @@ describe('generators', () => {
       document.body.appendChild(parentDiv);
 
       let xpathResult;
-      
+
       xpathResult = generators.getElementTreeXPath(childDiv1);
-      expect(xpathResult).to.be.equal('/html/body/div/div[1]', 'Can\'t find nested div appropriately.');
+      expect(xpathResult).to.be.equal(
+        '/html/body/div/div[1]',
+        "Can't find nested div appropriately."
+      );
 
       xpathResult = generators.getElementTreeXPath(childDiv2);
-      expect(xpathResult).to.be.equal('/html/body/div/div[2]', 'Can\'t find nested div appropriately.');
+      expect(xpathResult).to.be.equal(
+        '/html/body/div/div[2]',
+        "Can't find nested div appropriately."
+      );
 
       xpathResult = generators.getElementTreeXPath(parentDiv);
-      expect(xpathResult).to.be.equal('/html/body/div', 'Can\'t find non-leaf div appropriately.');
+      expect(xpathResult).to.be.equal(
+        '/html/body/div',
+        "Can't find non-leaf div appropriately."
+      );
 
       xpathResult = generators.getElementTreeXPath(document.body);
-      expect(xpathResult).to.be.equal('/html/body', 'Can\'t find body appropriately.');
+      expect(xpathResult).to.be.equal(
+        '/html/body',
+        "Can't find body appropriately."
+      );
     });
 
     it('should support returning XPath as a tree', () => {
@@ -48,40 +58,39 @@ describe('generators', () => {
       const parentDiv = document.createElement('div');
       const childDiv1 = document.createElement('div');
       const childDiv2 = document.createElement('div');
-      childDiv2.class = 'test';
+      childDiv2.className = 'test';
 
       parentDiv.appendChild(childDiv1);
       parentDiv.appendChild(childDiv2);
       document.body.appendChild(parentDiv);
 
       let xpathResult;
-      
+
       xpathResult = generators.getElementTreeXPath(childDiv1, false);
       expect(xpathResult).to.have.length(4);
       expect(xpathResult[0].tag).to.be.equal('html');
-
     });
   });
   describe('getElementAttributes', () => {
     it('should be able to get a single attribute from an element', () => {
-        const document = newDocument();
-        const childDiv2 = document.createElement('div');
-        childDiv2.align = 'center';
+      const document = newDocument();
+      const childDiv2 = document.createElement('div');
+      childDiv2.align = 'center';
 
-        const attributes = generators.getElementAttributes(childDiv2);
-        expect(attributes).to.have.property('align', 'center');
+      const attributes = generators.getElementAttributes(childDiv2);
+      expect(attributes).to.have.property('align', 'center');
     });
 
     it('should work in the no attribute case', () => {
-        const document = newDocument();
-        const div = document.createElement('div');
+      const document = newDocument();
+      const div = document.createElement('div');
 
-        const attributes = generators.getElementAttributes(div);
-        expect(attributes).to.be.empty;
+      const attributes = generators.getElementAttributes(div);
+      expect(attributes).to.be.empty;
     });
   });
   describe('getElementXPath', () => {
-      it('should work in simple DOM case', () => {
+    it('should work in simple DOM case', () => {
       const document = newDocument();
 
       const parentDiv = document.createElement('div');
@@ -95,14 +104,17 @@ describe('generators', () => {
       document.body.appendChild(parentDiv);
 
       let xpathResult;
-      
+
       xpathResult = generators.getElementXPath(childDiv2);
-      expect(xpathResult).to.be.equal('//*[@id="identifier"]', 'Expected straight ID lookup.');
+      expect(xpathResult).to.be.equal(
+        '//*[@id="identifier"]'
+      );
 
       xpathResult = generators.getElementXPath(childDiv2, true);
-      expect(xpathResult).to.be.equal('/html/body/div/div[2]', 'Expected non-ID lookup.');
+      expect(xpathResult).to.be.equal(
+        '/html/body/div/div[2]',
+        'Expected non-ID lookup.'
+      );
     });
-
   });
-
 });
